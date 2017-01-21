13786704810491662f0a43347799b503587dfbcd
kakuyogy <xuzhanddbb88@163.com>
2017-01-21 18:39:04 +0800

    go

--- ago.js
+++ ago.js
@@ -9,13 +9,29 @@
 //         module.exports = factory() :
 //         // amd
 //         typeof define === 'function' && define.amd ? define(factory) :
-//             (global.Ago = factory());
+//             (global.Vue = factory());
 // } (this, (function () { 
 'use strict';
 /*  */
 
+var uid = 0;
 
-function Ago(options) {
+function initMixin (Vue) {
+    Vue.prototype._init = function (options) {
+        var vm = this;
+        // a uid
+        vm._uid = uid++;
+        // a flag to avoid this being observed
+        vm._isVue = true;
+        // merge options
+        if (options && options._isComponent) {
+            
+        }
+    }
+}
+
+
+function Vue(options) {
     /*if(process.env.NODE_ENV !== 'prod' && 
     !(this instanceof Ago)) {
         warn('Ago is a constructor and should be called with the `new` keyword');
@@ -23,8 +39,8 @@ function Ago(options) {
     this._init(options);
 }
 
-initMixin(Ago);
+initMixin(Vue);
 
-return Ago;
+return Vue;
 
 // })))
